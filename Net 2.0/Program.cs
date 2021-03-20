5fc6d0aef7c41923c3fe2bf396551357862a3bd2
Никита Кравченко <KravchenkoNA@stud.kai.ru>
2021-03-20 16:34:57 +0300

    Леша Галоша

--- Net 2.0/Program.cs
+++ Net 2.0/Program.cs
@@ -17,6 +17,7 @@ namespace Net_2._0
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // леша собака сутулаая
         }
     }
 }
