0ed09733b0033caedec4d3b847ed958b3c986195
erkashov <47537711+erkashov@users.noreply.github.com>
2021-03-20 12:15:57 +0300

    Update Form1.cs

--- Net 2.0/Form1.cs
+++ Net 2.0/Form1.cs
@@ -10,13 +10,10 @@ using System.Windows.Forms;
 
 namespace Net_2._0
 {
-//не соси
     public partial class Form1 : Form
     {
-        //но все же соси
         public Form1()
         {
-        //сосать или не сосать вот в чом вопрос
             InitializeComponent();
         }
     }
